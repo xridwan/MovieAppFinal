230f89a87a1b3430ae08e81af50419506f648f47
xridwan <ridwan.native@gmail.com>
2022-05-20 00:04:33 +0700

    Update binding NavHostFragment

--- app/src/main/java/com/xridwan/moviecatalogue3/ui/main/MainActivity.kt
+++ app/src/main/java/com/xridwan/moviecatalogue3/ui/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.xridwan.moviecatalogue3.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
