8150b410af1e2b2075641a4a2f5a18cd4eb6d01d
Ridwan Ridwan <ridwan@crealoka.com>
2022-06-22 00:46:32 +0700

    Test

--- app/src/main/java/com/xridwan/moviecatalogue3/di/Injection.kt
+++ app/src/main/java/com/xridwan/moviecatalogue3/di/Injection.kt
@@ -6,7 +6,7 @@ import com.xridwan.moviecatalogue3.data.source.local.LocalDataSource
 import com.xridwan.moviecatalogue3.data.source.local.room.ContentDatabase
 import com.xridwan.moviecatalogue3.data.source.remote.RemoteDataSource
 import com.xridwan.moviecatalogue3.utils.AppExecutors
-
+//
 object Injection {
     fun provideRepository(context: Context): MovieCatalogueRepository {
         val database = ContentDatabase.getInstance(context)
